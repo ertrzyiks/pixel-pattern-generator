07c070ed45f523563f331f4e0efc95bf1239e792
Mateusz Derks <mateusz.derks@schibsted.pl>
2015-01-09 22:46:58 +0100

    refactor(js): extract box-shadow parser

--- dist/js/pixel-pattern-generator.js
+++ dist/js/pixel-pattern-generator.js
@@ -110,13 +110,62 @@
 (function(root) {
     "use strict";
     var PPG = root.PPG, dom = PPG.dom;
+    function PixelsParser(el) {
+        this.el = el;
+    }
+    PixelsParser.prototype.process = function() {
+        var boxShadow = dom.getComputedStyle(this.el, "box-shadow"), boxShadowItems, fontSize = this.getComputedFontSize_(), pixels = [], pixel, i;
+        boxShadow = this.corruptRgbColors_(boxShadow);
+        boxShadowItems = (boxShadow || "").split(",");
+        for (i = 0; i < boxShadowItems.length; i++) {
+            pixel = this.processComponent_(boxShadowItems[i]);
+            pixel.x = this.toEm_(pixel.x, fontSize);
+            pixel.y = this.toEm_(pixel.y, fontSize);
+            pixel.col = this.repairRgbColors_(pixel.col);
+            pixels.push(pixel);
+        }
+        return pixels;
+    };
+    PixelsParser.prototype.processComponent_ = function(component) {
+        var pixel = {}, componentParts = PPG.util.filter(component.split(" "), function(v) {
+            return v;
+        });
+        if (component.indexOf("rgb") === -1) {
+            pixel.x = componentParts[0];
+            pixel.y = componentParts[1];
+            pixel.col = componentParts.pop();
+        } else {
+            pixel.x = componentParts[1];
+            pixel.y = componentParts[2];
+            pixel.col = componentParts[0];
+        }
+        return pixel;
+    };
+    PixelsParser.prototype.toEm_ = function(value, fontSize) {
+        if (value.indexOf("px") !== -1) {
+            return parseInt(value, 10) / fontSize + "em";
+        }
+        return value;
+    };
+    PixelsParser.prototype.getComputedFontSize_ = function() {
+        var fontSizeValue = dom.getComputedStyle(this.el, "font-size"), fontSize = parseInt(fontSizeValue, 10);
+        return fontSize;
+    };
+    PixelsParser.prototype.corruptRgbColors_ = function(string) {
+        return string.replace(/rgb\([^\([^\)]+\)/g, function(match) {
+            return match.replace(/,/g, ";").replace(/\s/g, "");
+        });
+    };
+    PixelsParser.prototype.repairRgbColors_ = function(string) {
+        return string.replace(/;/g, ",");
+    };
     function Fallback(element) {
         this.el = element;
         this.pixel = PPG.dom.getChildByClass(this.el, "pixel-pattern-pixel");
         PPG.dom.addClass(this.el, "pixel-pattern-fallback");
     }
     Fallback.prototype.update = function() {
-        var pixels = this.getPixelsFrom_(this.pixel), pixelsNum = pixels.length, pixelEl, container = dom.getContainer(this.el, "pixel-pattern-fallback-container"), i;
+        var parser = new PixelsParser(this.pixel), pixels = parser.process(), pixelsNum = pixels.length, pixelEl, container = dom.getContainer(this.el, "pixel-pattern-fallback-container"), i;
         container = dom.removeChildren(container);
         container.style.position = "relative";
         for (i = 0; i < pixelsNum; i++) {
@@ -138,41 +187,6 @@
         el.style.width = "1em";
         el.style.height = "1em";
         return el;
-    };
-    Fallback.prototype.getPixelsFrom_ = function(el) {
-        var boxShadow = dom.getComputedStyle(el, "box-shadow"), fontSizeValue = dom.getComputedStyle(el, "font-size"), fontSize = parseInt(fontSizeValue, 10), pixels = [], boxShadowItems, boxShadowItem, boxShadowItemParts, pixel, i;
-        boxShadow = this.tweakRgbColors_(boxShadow);
-        boxShadowItems = (boxShadow || "").split(",");
-        for (i = 0; i < boxShadowItems.length; i++) {
-            boxShadowItem = boxShadowItems[i];
-            boxShadowItemParts = PPG.util.filter(boxShadowItem.split(" "), function(v) {
-                return v;
-            });
-            pixel = {};
-            if (boxShadowItem.indexOf("rgb") === -1) {
-                pixel.x = boxShadowItemParts[0];
-                pixel.y = boxShadowItemParts[1];
-                pixel.col = boxShadowItemParts.pop();
-            } else {
-                pixel.x = boxShadowItemParts[1];
-                pixel.y = boxShadowItemParts[2];
-                pixel.col = boxShadowItemParts[0];
-            }
-            if (pixel.x.indexOf("px") !== -1) {
-                pixel.x = parseInt(pixel.x, 10) / fontSize + "em";
-            }
-            if (pixel.y.indexOf("px") !== -1) {
-                pixel.y = parseInt(pixel.y, 10) / fontSize + "em";
-            }
-            pixel.col = pixel.col.replace(/;/g, ",");
-            pixels.push(pixel);
-        }
-        return pixels;
-    };
-    Fallback.prototype.tweakRgbColors_ = function(string) {
-        return string.replace(/rgb\([^\([^\)]+\)/g, function(match) {
-            return match.replace(/,/g, ";").replace(/\s/g, "");
-        });
     };
     PPG.setupFallback = function(el) {
         if (!PPG.isEnabled()) {
